80dba94106ff09f09006f0aad0af6b27fe0712c8
Emanuela Cardu <emanuelacardu@gmail.com>
2023-12-13 15:50:00 +0100

    après online

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 const btn_phpImage = document.querySelector("#phpImage");
 btn_phpImage.addEventListener("click", function(){
     const showed_art = document.querySelector('.art-1');
-    showed_art.classList.remove("hidden");
+    showed_art.classList.toggle("hidden");
 
 })
+
+
